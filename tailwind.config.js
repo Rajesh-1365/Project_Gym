e7e281fac455031b58f834b234b2c50e59f2f860
Rajesh-1365 <rbusala@gitam.in>
2024-09-15 09:52:36 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"],
+    theme: {
+      extend: {
+        colors: {
+          "logo-color": "#0F0D1A",
+        },
+      },
+    },
+    plugins: [],
+  };
